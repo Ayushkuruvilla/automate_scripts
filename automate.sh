be20b0be53e06463db5623c4ff3ee49e5e04abb9
Ayush Kuruvilla <51397154+Ayushkuruvilla@users.noreply.github.com>
2021-06-04 20:00:37 +0530

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+sudo apt-get install -y snapd
+sudo snap install bitcoin-core
+ln -s /snap/bitcoin-core/current/bin/bitcoin{d,-cli} /usr/local/bin/
+cd /home/ayush
 sudo apt-get update
 sudo apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python3 python3-mako net-tools zlib1g-dev libsodium-dev gettext
 git clone https://github.com/ElementsProject/lightning.git
@@ -9,9 +14,16 @@ make
 sudo make install
 cd ..
 mkdir .bitcoin
-mv /home/ayush/automate_scripts/bconfig /home/ayush/.bitcoin/
+git clone https://github.com/Ayushkuruvilla/automate_scripts.git
+mv /home/ayush/automate_scripts/bconfig /home/ayush/.bitcoin/config
 mkdir .lightning
 mv /home/ayush/automate_scripts/config /home/ayush/.lightning/
+pub=$(dig +short myip.opendns.com @resolver1.opendns.com)
+internal=$(hostname -I | awk '{print $1}')
+var3="bind-addr="$internal":9735"
+var4="announce-addr="$pub":9735"
+echo $var3 >> .lightning/config
+echo $var4 >> .lightning/config
 mkdir .lightning/testnet
 touch .lightning/testnet/se.txt
 gcloud secrets versions access 1 --secret="my-secret" >> .lightning/testnet/se.txt
